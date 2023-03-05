600397c9638df6fde1f8509ba58104b1678379d0
blau <le.space@icloud.com>
2023-03-05 16:08:29 +0900

    Array型

--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
-const person: {
-  name: string;
-  age: 30;
-} = {
+const person = {
   name: 'yota',
   age: 30,
+  hobbies: ['Sports', 'Cooking'],
 }
 
+let favoriteActivities: string[];
+favoriteActivities = ['sports'];
+
 console.log(person.name);
+
+for (const hobboy of person.hobbies) {
+  console.log(hobboy.toUpperCase());
+}
