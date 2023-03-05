490e430ff8a59bc8a8883c22d7161d782000ba30
blau <le.space@icloud.com>
2023-03-06 00:14:49 +0900

    function型

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ function add(num1, num2) {
 function printResult(num) {
     console.log(num);
 }
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
 printResult(add(12, 5));
--- app.ts
+++ app.ts
@@ -6,4 +6,12 @@ function printResult(num: number): void {
   console.log(num);
 }
 
+let combineValues: (a :number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
+
 printResult(add(12, 5));
