06b42c857a0172cb1fece1b7ee48cdaeda7c1689
blau <le.space@icloud.com>
2023-03-06 00:20:58 +0900

    function型とコールバック

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ function add(num1, num2) {
 function printResult(num) {
     console.log(num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
 var combineValues;
 combineValues = add;
 // combineValues = printResult;
