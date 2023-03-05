5701e06f027d2b76fbd25df68f05628f01291ce9
blau <le.space@icloud.com>
2023-03-05 23:02:08 +0900

    Enum型

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'yota',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],  // array
+//   role: [2, 'author'],             // tuple
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'yota',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
-person.role[1] = 10;
+// person.role.push('admin'); こっちは許可される
+// person.role[1] = 10;
+// person.role = [0, 'admin'];
 var favoriteActivities;
 favoriteActivities = ['sports'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobboy = _a[_i];
     console.log(hobboy.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log('管理者ユーザー');
 }
--- app.ts
+++ app.ts
@@ -1,14 +1,28 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'yota',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],  // array
+//   role: [2, 'author'],             // tuple
+// }
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR
+}
+
+const person = {
   name: 'yota',
   age: 30,
   hobbies: ['Sports', 'Cooking'],  // array
-  role: [2, 'author'],             // tuple
+  role: Role.ADMIN
 }
+
 
 // person.role.push('admin'); こっちは許可される
 // person.role[1] = 10;
@@ -22,4 +36,8 @@ console.log(person.name);
 
 for (const hobboy of person.hobbies) {
   console.log(hobboy.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('管理者ユーザー');
 }
