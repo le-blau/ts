54216706c8e495e7c2370a84b5fc00a663375fe8
blau <le.space@icloud.com>
2023-03-12 18:28:07 +0900

    prj: first

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('aaabbcc');
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement; 
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;  // #project-input のフォーム要素
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
