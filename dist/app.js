0d8e964f47fc8771adad6102b61721d920829209
blau <le.space@icloud.com>
2023-03-26 21:47:26 +0900

    Getterの利用

--- dist/app.js
+++ dist/app.js
@@ -102,6 +102,14 @@ class Component {
     }
 }
 class ProjectItem extends Component {
+    get manday() {
+        if (this.project.manday < 20) {
+            return this.project.manday.toString() + "人日";
+        }
+        else {
+            return (this.project.manday / 20).toString() + "人月";
+        }
+    }
     constructor(hostId, project) {
         super("single-project", hostId, false, project.id);
         this.project = project;
@@ -111,8 +119,7 @@ class ProjectItem extends Component {
     configure() { }
     renderContent() {
         this.element.querySelector("h2").textContent = this.project.title;
-        this.element.querySelector("h3").textContent =
-            this.project.manday.toString();
+        this.element.querySelector("h3").textContent = this.manday;
         this.element.querySelector("p").textContent = this.project.description;
     }
 }
